ec45583f73ddb91066a47e5fcf7998976845fb22
Vaibhav Shukla <vaibhav.shuklaoct@gmail.com>
2022-09-11 20:37:57 +0530

    Add products component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className='hero'>
-            <div class="card text-bg-dark">
-                <img src="/assets/background-shopping2.jpg" class="card-img" alt="background" height="620px" />
-                <div class="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="card text-bg-dark">
+                <img src="/assets/background-shopping2.jpg" className="card-img" alt="background" height="620px" />
+                <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
-                        <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-                        <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+                        <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                        <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
                     </div>
                 </div>
             </div>
+            <Products/>
         </div>
     )
 }
