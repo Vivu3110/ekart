2ab88a1c85fa48a731b166386981e37f7f02458b
Vaibhav Shukla <vaibhav.shuklaoct@gmail.com>
2022-09-11 21:19:09 +0530

    Use React Router to Route to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Home from './component/Home';
+import Products from './component/Products';
 import Navbar from './component/Navbar';
-
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Home/>  
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route  path="/products" element={<Products/>}/> 
+      </Routes>
     </>
   );
 }
