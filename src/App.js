d92fab518124782d5336adb83ccfb4dcd54dc443
Vaibhav Shukla <vaibhav.shuklaoct@gmail.com>
2022-09-15 00:12:23 +0530

    Add a product page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './component/Home';
 import Products from './component/Products';
 import Navbar from './component/Navbar';
 import {Routes, Route} from 'react-router-dom';
+import Product from './component/Product';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route  path="/" element={<Home/>}/>
         <Route  path="/products" element={<Products/>}/> 
+        <Route  path="/products/:id" element={<Product/>}/> 
       </Routes>
     </>
   );
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from "react";
-import Skeleton from 'react-loading-skeleton'
+import Skeleton from 'react-loading-skeleton';
+import { NavLink } from "react-router-dom";
 
 const Products = () => {
 
@@ -17,7 +18,6 @@ const Products = () => {
                 setData(await response.clone().json());
                 setFilter(await response.json());
                 setLoading(false);
-                console.log(filter);
             }
 
             return () => {
@@ -65,12 +65,12 @@ const Products = () => {
                     return (
                         <>
                             <div className="col-md-3 mb-4">
-                                <div class="card h-100 text-center p-4" key={product.id}>
-                                    <img src={product.image} class="card-img-top" alt={product.title} height="250px" />
-                                    <div class="card-body">
-                                        <h5 class="card-title mb-0">{product.title.substring(0,12)}...</h5>
-                                        <p class="card-text lead fw-bold">${product.price}</p>
-                                        <a href="#" class="btn btn-outline-dark">Buy Now</a>
+                                <div className="card h-100 text-center p-4" key={product.id}>
+                                    <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
+                                    <div className="card-body">
+                                        <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
+                                        <p className="card-text lead fw-bold">${product.price}</p>
+                                        <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
                                     </div>
                                 </div>
                             </div>
